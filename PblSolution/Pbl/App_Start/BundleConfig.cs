34b11ef9e2daba64beede432262d617c6a1d8aeb
Mateus Schenatto <mateus.schenatto@live.com>
2017-05-08 15:06:27 -0300

    Tela de login finalizada

--- PblSolution/Pbl/App_Start/BundleConfig.cs
+++ PblSolution/Pbl/App_Start/BundleConfig.cs
@@ -30,7 +30,6 @@ namespace Pbl
                       "~/Content/custom.css",
                       "~/Content/font-awesome.css",
                       "~/Content/site.css",
-                      "~/Content/login.css",
                       "~/Content/DataTables/css/jquery.dataTables.min.css"));
         }
     }
