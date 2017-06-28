0e86ecbd668dc1847d1af51b22deea850dd0370a
Lucas Silva Guimarães <lucas.rv@hotmail.com>
2017-06-28 17:58:55 -0300

    modulo Aluno, primeiro commit

---
+++ PblSolution/Pbl/Controllers/AcompanhamentoAlunoController.cs
@@ -0,0 +1,32 @@
+﻿using Pbl.Models;
+using Pbl.Models.DbClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pbl.Controllers
+{
+    public class AcompanhamentoAlunoController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            Usuario user = (Usuario)Session["usuario"];
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            Aluno aluno = user.Aluno.First();
+            List<InscricaoTurma> inscricoesEmTurmas = aluno.InscricaoTurma.ToList();
+            return View(inscricoesEmTurmas);
+        }
+
+        public ActionResult AcompanharMed(int idInscricaoTurma, int idMed)
+        {
+            List<ControleNotas> listControleNotas = new MControleNotas().Bring(c => (c.idInscricaoTurma == idInscricaoTurma));
+            return View(listControleNotas);
+        }
+    }
+}
