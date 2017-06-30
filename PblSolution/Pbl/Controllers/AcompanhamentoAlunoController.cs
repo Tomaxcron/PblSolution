1cca711a346d1b57e365f3c844917879e0565646
Lucas Silva Guimarães <lucas.rv@hotmail.com>
2017-06-30 15:20:25 -0300

    Calculo de notas no controller do aluno

--- PblSolution/Pbl/Controllers/AcompanhamentoAlunoController.cs
+++ PblSolution/Pbl/Controllers/AcompanhamentoAlunoController.cs
@@ -28,6 +28,15 @@ namespace Pbl.Controllers
             List<ControleNotas> listControleNotas = new MControleNotas().Bring(c => (c.idInscricaoTurma == idInscricaoTurma));
             int numDisciplinasPraticas = new MMed().BringOne(c => c.idMed == idMed).Disciplina.Where(c => c.idTipoDisciplina == 2).Count();
             int numDisciplinasMorfofuncionais = new MMed().BringOne(c => c.idMed == idMed).Disciplina.Where(c => c.idTipoDisciplina == 1).Count();
+            
+            foreach (ControleNotas modulo in listControleNotas)
+            {
+                var disciplinasPraticas = modulo.ControleNotasXAula.Where(c => c.Aula.Disciplina.idTipoDisciplina == 2).Sum(c => c.nota);
+                var disciplinasMorfofuncionais = modulo.ControleNotasXAula.Where(c => c.Aula.Disciplina.idTipoDisciplina == 1).Sum(c => c.nota);
+                var ProvaoTutoria = modulo.ControleNotasXProva.Where(c => c.Prova.idTipoProva == 1).Select(c => (c.Prova.valorQuestao * c.numAcertos));
+                var ProvaoPratica = modulo.ControleNotasXProva.Where(c => c.Prova.idTipoProva == 2).Select(c => (c.Prova.valorQuestao * c.numAcertos));
+                
+            }
             return View(listControleNotas);
         }
     }
